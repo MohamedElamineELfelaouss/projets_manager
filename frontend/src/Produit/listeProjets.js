dde33619b67d637582f6ec7335e3a07fa7246ead
EL Felaouss mohamed elamine <elfelaoussmohamedelamine@gmail.com>
2025-03-25 11:46:10 +0000

    push2

--- frontend/src/Produit/listeProjets.js
+++ frontend/src/Produit/listeProjets.js
@@ -18,6 +18,26 @@ export default function ListeProjets() {
   return (
     <div>
       <h1>Liste des Projets</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>nom Projet</th>
+            <th>Projet description</th>
+            <th>photo Projet</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projets.map((projet) => (
+            <tr key={projet.id}>
+              <td>{projet.id}</td>
+              <td>{projet.nomProjet}</td>
+              <td>{projet.description}</td>
+              <td>{projet.photoProjet}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
